3da05b412c36e90bdc9a95c6f9857bbc51514bfd
Lukas Gaechter <lukas.gaechter@mind.ch>
2023-10-28 21:25:20 +0200

    Switch to integer for {{ version }}

--- _data/site.js
+++ _data/site.js
@@ -7,12 +7,14 @@ module.exports = {
       : 'https://timber.github.io',
   versions: [
     {
+      value: 2,
       name: 'v2.x',
       slug: 'v2',
       link: '/v2/',
       glob: './content/v2/**',
     },
     {
+      value: 1,
       name: 'v1.x',
       slug: 'v1',
       link: '/v1/',
