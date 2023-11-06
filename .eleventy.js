84f456903e8abda2c2befa8a6553152e03f09187
Lukas Gaechter <lukas.gaechter@mind.ch>
2023-11-07 00:01:05 +0100

    Move redirectCollection to separate function

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,7 @@ const selectChildren = require('./lib/selectChildren');
 const pluginRss = require('@11ty/eleventy-plugin-rss');
 const options = require('./_data/options');
 const site = require('./_data/site');
+const redirectCollection = require('./lib/redirectCollection');
 
 module.exports = function (config) {
   // Copy folders and files.
@@ -66,24 +67,7 @@ module.exports = function (config) {
 
     if (version.slug === 'v1') {
       config.addCollection('v1redirects', function (collection) {
-        let redirects = collection.getFilteredByGlob(version.glob);
-
-        // Filter out index.
-        redirects = redirects.filter(
-          (item) => item.filePathStem !== '/v1/index',
-        );
-
-        redirects = redirects.map((item) => {
-          // Previously, the v1 docs lived in a non-versioned folder (e.g. timber.github.io/docs/getting-started/setup/). Now, we take the v1 URLs as a basis for redirects from
-          // that no-versioned folder to the latest version. This might lead to 404s, but we will
-          // live with that for the moment.
-          return {
-            from: item.url.replace('/v1', ''),
-            to: item.url.replace('/v1', `/${site.versions[0].slug}`),
-          };
-        });
-
-        return redirects;
+        return redirectCollection(collection, version.glob, site);
       });
     }
   });
