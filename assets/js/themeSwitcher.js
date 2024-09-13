5e51caab3a9cc3104e8d73999ab30a550a93ce1f
Brandon Zhang <gwonzhang@gmail.com>
2024-09-13 14:48:02 +0700

    Add feature to toggle between light mode and dark mode `wip` (#83)
    * Feat: add dark/lightmode to docs website
    
    ---------
    
    Co-authored-by: Erik van der Bas <erik@basedonline.nl>

---
+++ assets/js/themeSwitcher.js
@@ -0,0 +1,20 @@
+window.switchTheme = () => {
+	const isDark = window.localStorage.getItem('theme') === 'dark';
+	document.documentElement.setAttribute('data-theme', isDark ? 'light' : 'dark');
+
+	localStorage.setItem("theme", document.documentElement.getAttribute('data-theme'));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	const theme = (() => {
+		if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+			return localStorage.getItem('theme');
+		}
+		if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			return 'dark';
+		}
+		return 'light';
+	})();
+	document.documentElement.setAttribute('data-theme', theme);
+	window.localStorage.setItem('theme', theme);
+});
